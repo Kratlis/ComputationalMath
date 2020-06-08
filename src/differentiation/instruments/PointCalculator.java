d1c2e22b1045a3bdd3b8f991b0fb0b7627e2f261
Ekaterina Kubikova <48138888+Kratlis@users.noreply.github.com>
2020-06-08 18:09:39 +0300

    Reformatted code. Changed step calculation.

--- src/differentiation/instruments/PointCalculator.java
+++ src/differentiation/instruments/PointCalculator.java
@@ -8,31 +8,28 @@ public class PointCalculator {
     private final ArrayList<Double> X;
     private final ArrayList<Double> Y;
     private final Function function;
-    private double step;
+    private final double step;
 
     public PointCalculator(double x0, double y0, double xn, double accuracy, Function function) {
-        this.step = (xn - x0) / 2;
+        this.step = Math.pow(accuracy, 0.25);
         this.function = function;
         X = new ArrayList<>();
         Y = new ArrayList<>();
         X.add(x0);
         Y.add(y0);
-        double y_next = getNextYValue(x0, y0);
-        while ((y_next - y0) > accuracy) {
-            step /= 2;
-            y_next = getNextYValue(x0, y0);
-        }
-        double x1 = x0 + step;
-        X.add(x1);
-        Y.add(y_next);
-        for (double i = x0 + step; i < xn; i += step) {
-            y_next = getNextYValue(i, y_next);
+        calculateCoordinates(x0, y0, xn);
+    }
+
+    private void calculateCoordinates(double x0, double y0, double xn) {
+        double y_next = y0;
+        for (double i = x0; i < xn; i += step) {
+            y_next = calculateNextYValue(i, y_next);
             X.add(i + step);
             Y.add(y_next);
         }
     }
 
-    private double getNextYValue(double x_prev, double y_prev) {
+    private double calculateNextYValue(double x_prev, double y_prev) {
         double k0, k1, k2, k3, delta;
         k0 = function.getValue(x_prev, y_prev);
         k1 = function.getValue(x_prev + step / 2, y_prev + k0 * step / 2);
